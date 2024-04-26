746a596d35404ceead31d9404e1113eef25f8f9a
Ba9A-tata <yogadzakkii@gmail.com>
2024-04-26 14:08:17 +0700

    menambahkan while

--- 089_singlelinkedlist/089_singlelinkedlist.cpp
+++ 089_singlelinkedlist/089_singlelinkedlist.cpp
@@ -12,4 +12,29 @@ Node* START = NULL;
 void addNode() {
 	int nim;
 	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukan NIM: ";
+	cin >> nim;
+	cout << "Masukan Nama: ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
+
+	Node* previos = START;
+	Node* current = START;
+	
+	while ((current != NULL) && (nim >= current->noMhs))
+
 }
